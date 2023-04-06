0a1dca60de07d840e9ba21e63eb173c0e6348988
gkgopesh15 <123261232+gkgopesh15@users.noreply.github.com>
2023-04-06 22:13:25 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,472 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Portfolio Website</title>
+
+    <!-- Swipper CSS -->
+    <link
+  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+
+    <!-- box icons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!--header design-->
+    <header class="header">
+    <a href="#" class="logo">Portfolio</a>
+
+    <nav class="navbar">
+        <a href="#home" class="active">Home</a>
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#contact">Contact</a>
+
+
+    </nav>
+
+    <div class="bx bx-moon" id="darkMode-icon"></div>
+
+    <div class="bx bx-menu" id="menu-icon"></div>
+    </header>
+
+    <!-- Home section Design -->
+
+    <section class="home" id="home">
+
+    <div class="home-content">
+        <h3>Hello, I am</h3>
+        <h1>Gopesh Kumar</h1>
+        <p>Working as a full stack developer at BGP Consultancy Services.
+            Continuesly learning and growing as a professional.
+            Always ready to work hard and taking risk for better career.
+        </p>
+
+         <div class="social-media">
+            <a href="https://www.facebook.com/profile.php?id=100008480672012&mibextid=ZbWKwL" target="_blank"><i class='bx bxl-facebook' ></i></a>
+            <a href="https://twitter.com/gkgopesh15?t=iwff4kgbAplEE9RuRF3xPQ&s=09" target="_blank"><i class='bx bxl-twitter' ></i></a>
+            <a href="https://instagram.com/raj.gopesh?igshid=ZDdkNTZiNTM=" target="_blank"><i class='bx bxl-instagram-alt' ></i></a>
+            <a href="https://www.linkedin.com/in/gopesh-kumar1503" target="_blank"><i class='bx bxl-linkedin' ></i></a>
+            <a href="https://youtube.com/@drdevelopers8991" target="_blank"><i class='bx bxl-youtube'></i></a>
+         </div>
+
+         <a href="cv/MyResume.pdf" class="btn" download>Download CV</a>
+    </div>
+
+    <div class="profession-container">
+        <div class="profession-box">
+            <div class="profession" style="--i:0" >
+            <i class='bx bx-code-alt'></i>
+            <h3>Web Developer</h3>
+            </div>
+            <div class="profession" style="--i:1">
+            <i class='bx bxl-youtube' ></i>
+            <h3>YouTuber</h3>
+            </div>
+            <div class="profession" style="--i:2">
+            <i class='bx bx-palette' ></i>
+            <h3>Web Designer</h3>
+            </div>
+            <div class="profession" style="--i:3">
+            <i class='bx bx-chalkboard' ></i>
+            <h3>Teacher</h3>
+            </div>
+            <div class="circle"></div>
+        </div>
+            <div class="overlay"></div>
+        </div>
+
+        <div class="home-img">
+            <img src="images/me2.png" alt="">
+        </div>
+
+    </section>
+
+    <!-- About Section Design -->
+
+    <section class="about" id="about">
+            <div class="about-img">
+                <img src="images/myimg.png" alt="">
+            </div>
+
+            <div class="about-content">
+                <h2 class="heading">About <span>Me</span></h2>
+                <h3>Let's Introduce My Self</h3>
+                <p>As a software developer and YouTuber, my portfolio showcases a unique blend of technical and creative skills. My proficiency in coding languages such as Java, Python, and C++ is demonstrated through various software projects that I have developed for a range of industries. In addition, my YouTube channel features engaging and informative content that covers topics related to software development, technology trends, and best practices.
+
+Through my portfolio, I aim to showcase my ability to effectively communicate technical information in a clear and concise manner, as well as my passion for staying up-to-date with the latest developments in the field of software development. My portfolio highlights my experience in project management, problem-solving, and collaboration, as well as my ability to consistently deliver high-quality work.
+
+Overall, my portfolio demonstrates my commitment to both the technical and creative aspects of software development and content creation, as well as my dedication to continuous learning and improvement.</p>
+
+                <a href="" class="btn">Read More</a>
+            </div>
+    </section>
+
+
+    <!-- Skills Section Design -->
+    <section>
+        <div class="skills_section">
+            <div class="skills_head">
+                    <h2>My <span>Skills</span></h2>
+                    <p>Here is my skills to represent my Expertise</p>
+            </div>
+
+        <div class="skills_main">
+            <div class="skill_bar">
+                <div class="info">
+                    <p>HTML</p>
+                    <P>90%</P>
+                </div>
+                <div class="bar">
+                    <span class="html"></span>
+                </div>
+            </div>
+            <div class="skill_bar">
+                <div class="info">
+                    <p>CSS</p>
+                    <P>75%</P>
+                </div>
+                <div class="bar">
+                    <span class="css"></span>
+                </div>
+            </div>
+            <div class="skill_bar">
+                <div class="info">
+                    <p>MySql</p>
+                    <P>70%</P>
+                </div>
+                <div class="bar">
+                    <span class="sql"></span>
+                </div>
+            </div>
+            <div class="skill_bar">
+                <div class="info">
+                    <p>Java Script</p>
+                    <P>80%</P>
+                </div>
+                <div class="bar">
+                    <span class="js"></span>
+                </div>
+            </div>
+            <div class="skill_bar">
+                <div class="info">
+                    <p>React js</p>
+                    <P>75%</P>
+                </div>
+                <div class="bar">
+                    <span class="react"></span>
+                </div>
+            </div>
+            <div class="skill_bar">
+                <div class="info">
+                    <p>Node js</p>
+                    <P>70%</P>
+                </div>
+                <div class="bar">
+                    <span class="node"></span>
+                </div>
+            </div>
+            <div class="skill_bar">
+                <div class="info">
+                    <p>Express js</p>
+                    <P>65%</P>
+                </div>
+                <div class="bar">
+                    <span class="express"></span>
+                </div>
+            </div>
+            <div class="skill_bar">
+                <div class="info">
+                    <p>Mongo DB</p>
+                    <P>60%</P>
+                </div>
+                <div class="bar">
+                    <span class="mongo"></span>
+                </div>
+            </div>
+
+            <div class="skill_bar">
+                <div class="info">
+                    <p>Java</p>
+                    <P>75%</P>
+                </div>
+                <div class="bar">
+                    <span class="java"></span>
+                </div>
+            </div>
+            <div class="skill_bar">
+                <div class="info">
+                    <p>PHP</p>
+                    <P>80%</P>
+                </div>
+                <div class="bar">
+                    <span class="php"></span>
+                </div>
+            </div>
+
+            <div class="skill_bar">
+                <div class="info">
+                    <p>DSA</p>
+                    <P>60%</P>
+                </div>
+                <div class="bar">
+                    <span class="dsa"></span>
+                </div>
+            </div>
+
+            <div class="skill_bar">
+                <div class="info">
+                    <p>Python</p>
+                    <P>65%</P>
+                </div>
+                <div class="bar">
+                    <span class="python"></span>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+   
+    
+</section>
+    
+    <!-- Services Section Design -->
+
+    <section class="services" id="services">
+
+        <h2 class="heading">My <span>Services</span></h2>
+
+        <div class="services-container">
+            <div class="services-box">
+            <i class='bx bx-code-alt'></i>
+                <h3>Web Developement</h3>
+                <p>I'm Working as a Web Developer in BGP Consultancy Services. I have 
+                    all the skills which are mentioned above. I have done various projects using HTML, CSS
+                JavaScripts, React Js, PHP ...</p>
+                <a href="#" class="btn">Read More</a>
+
+            </div>
+
+            <div class="services-box">
+                <i class='bx bxs-chalkboard' ></i>
+                <h3>Developement Training</h3>
+                <p>I have been providing training and education on various software 
+                    applications for 2 years. My areas of expertise include in Web Developement and Desktop Application
+                     Developement...</p>
+                <a href="#" class="btn">Read More</a>
+                
+            </div>
+
+            <div class="services-box">
+                <i class='bx bxs-edit-alt'></i>
+                <h3>Freelancing</h3>
+                <p> I am a freelance 'Web Developement, Blog Writting, Video Editing. I have 3 years of 
+                    experience i the field.  I am confident in my ability to provide high-quality and effective solutions 
+                    to meet the needs of my clients...</p>
+                <a href="#" class="btn">Read More</a>
+                
+            </div>
+        </div>
+
+    </section>
+
+
+    <!-- Portfolio section design-->
+
+    <section class="portfolio" id="portfolio">
+
+        <h2 class="heading">Latest <span>Project</span></h2>
+
+        <div class="portfolio-container">
+            <div class="portfolio-box">
+                <img src="images/prg1.png" alt="">
+
+                <div class="portfolio-layer">
+                    <h4>Paridhi NGO</h4>
+                    <p>A Website of Paridhi NGO. Developed using HTML, CSS, JavaScript,
+                         Bootstrap, PHP and MySQL</p>
+                    <a href="https://paridhi.org.in/" target="_blank"><i class='bx bx-link-external' ></i></a>
+                </div>
+            </div>
+
+            <div class="portfolio-box">
+                <img src="images/prg2.png" alt="">
+
+                <div class="portfolio-layer">
+                    <h4>Fit Club Gym</h4>
+                    <p>A GYM Website which is completely developed using React Js , CSS , HTML.</p>
+                    <a href="https://fitclubgopeshraj.pages.dev/" target="_blank"><i class='bx bx-link-external' ></i></a>
+                </div>
+            </div>
+
+            <div class="portfolio-box">
+                <img src="images/prg3.png" alt="">
+
+                <div class="portfolio-layer">
+                    <h4>Portfolio</h4>
+                    <p>An Amazing, responsive portfolio Website which is developed using HTML, CSS , JavaScript, SwipperJs etc </p>
+                    <a href="#"><i class='bx bx-link-external' ></i></a>
+                </div>
+            </div>
+
+            <div class="portfolio-box">
+                <img src="images/prg4.png" alt="">
+
+                <div class="portfolio-layer">
+                    <h4>Netflix Clone</h4>
+                    <p>Clone of Netflix which is developed using MERN stacks technology.</p>
+                    <a href="#"><i class='bx bx-link-external' ></i></a>
+                </div>
+            </div>
+
+            <div class="portfolio-box">
+                <img src="images/prg5.png" alt="">
+
+                <div class="portfolio-layer">
+                    <h4>GAS Agency </h4>
+                    <p>A Desktop Application Developed in Visual Basic which is used to manage 
+                        all activity of a GAS Agency Services.
+                    </p>
+                    <a href="https://github.com/gkgopesh15/GasAgency.git" target="_blank"><i class='bx bx-link-external' ></i></a>
+                </div>
+            </div>
+
+            <div class="portfolio-box">
+                <img src="images/prg6.png" alt="">
+
+                <div class="portfolio-layer">
+                    <h4>Bakery Cafe</h4>
+                    <p>A Windows Application which is used by a Bakery shop. Developed in Visual C# </p>
+                    <a href="https://github.com/gkgopesh15/BakeryCafe.git" target="_blank"><i class='bx bx-link-external' ></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Testimonial section Design-->
+
+    <div class="testimonial-container ">
+        <h2 class="heading">Valuable <span>Testimonial</span></h2>
+
+        <div class="testimonial-wrapper">
+            <div class="testimonial-box  mySwiper">
+                <div class="testimonial-content swiper-wrapper">
+                    <div class="testimonial-slide swiper-slide">
+                        <img src="images/society.png" alt="">
+                        <h3>Team Paridhi</h3>
+                        <p>Very Good Services provided by develioper From my interaction with the website, I must say that it is visually appealing,
+                            user-friendly, and responsive. The website is easy to navigate, and all the 
+                            essential information about your NGO is readily available. The layout and design
+                            are well-thought-out, and the website's color scheme complements your brand perfectly.
+                            The functionality of the website is impressive, and the developer has 
+                            done a great job of incorporating all the necessary features. The website's 
+                            speed is fast, and it loads quickly, ensuring that users do not experience
+                             any delays.
+                        </p>
+                    </div>
+
+                    <div class="testimonial-slide swiper-slide">
+                        <img src="images/test1.jpeg" alt="">
+                        <h3>Vikash Kumar</h3>
+                        <p>An Amaizing trainer, I have completed my 3 semester of BCA with sir.
+                            Their teaching style is effective, and they have an in-depth understanding of the 
+                            subject matter, a great ability to explain complex concepts
+                             in a clear and
+                             concise manner, making it easy for me to understand and apply in my software 
+                             development projects. They are patient and willing to provide additional
+                              explanations and support when needed, which has helped me to 
+                            overcome any challenges I may have encountered during the training process.
+                        </p>
+                    </div>
+
+                    <div class="testimonial-slide swiper-slide">
+                        <img src="images/test2.jpeg" alt="">
+                        <h3>Deepak Singh</h3>
+                        <p>Very good knowledge of various technologies. 
+                            I have completed my various cources in the guidence of Gopesh Sir.
+                            their teaching style is effective, and they have an in-depth understanding 
+                            of the subject matter.
+                                they  has a great ability to explain complex concepts in a clear and
+                             concise manner, making it easy for you to understand and apply in my software
+                              development projects. They are patient and willing to provide additional 
+                              explanations and support when needed, which has helped me to overcome any 
+                            challenges I may have encountered during the training process.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-pagination"></div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Contact Section Design -->
+
+    <section class="contact" id="contact">
+        <h2 class="heading">Contact <span>Me</span></h2>
+
+        <form action="#">
+            <div class="input-box">
+                <input type="text" placeholder="Full Name">
+                <input type="email" placeholder="Email Address">
+
+            </div>
+
+            <div class="input-box">
+                <input type="number" placeholder="Mobile Number">
+                <input type="text" placeholder="Email Subject">
+                
+            </div>
+
+            <textarea name="" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
+            <input type="submit" value="Send Message" class="btn">
+        </form>
+    </section>
+
+    <!-- Footer Design -->
+
+    <div class="footer">
+        <div class="footer-text">
+            <p>Copyright &copy; 2023 by Gopesh Kumar | All Rights Reserved</p>
+        </div>
+
+        <div class="footer-iconTop">
+            <a href="#home"><i class='bx bx-up-arrow-alt'></i></a>
+        </div>
+    </div>
+
+
+
+
+   
+
+
+    <!--    Scroll Reveal  -->
+    <script src="https://unpkg.com/scrollreveal"></script>
+
+
+
+    <!-- Swiperjs -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
